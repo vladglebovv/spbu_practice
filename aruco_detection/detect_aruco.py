93031c4a8c8f2bbfff3842f01e810d1b0e3da802
DaniilDDDDD <daniilpanyushin@yandex.ru>
2021-11-10 22:02:26 +0300

    Added generation of ArUco markers and detection of them.

---
+++ aruco_detection/detect_aruco.py
@@ -0,0 +1,99 @@
+import cv2
+import argparse
+import sys
+import imutils
+
+
+ARUCO_DICT = {
+	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+	"DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+	"DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+	"DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+	"DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+	"DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+	"DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+	"DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+	"DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+	"DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+	"DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+	"DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+	"DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+	"DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+	"DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+	"DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+	"DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+}
+
+
+if __name__ == '__main__':
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-t", "--type", type=str,
+        default="DICT_ARUCO_ORIGINAL",
+        help="type of ArUCo tag to detect")
+    ap.add_argument(
+        "-c", "--camera",
+        type=int,
+        default=0,
+        required=True,
+    help="camera id in system")
+    args = vars(ap.parse_args())
+
+    if ARUCO_DICT.get(args["type"], None) is None:
+        print("[INFO] ArUCo tag of '{}' is not supported".format(
+            args["type"]))
+        sys.exit(0)
+
+
+    try:
+        cap = cv2.cap = cv2.VideoCapture(args['c'])
+    except Exception:
+        print("Wrong camera!")
+        sys.exit(0)
+
+    arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[args["type"]])
+    arucoParams = cv2.aruco.DetectorParameters_create()
+
+    while True:
+        ret, img = cap.read()
+        img = imutils.resize(img, width=1000)
+        (corners, ids, rejected) = cv2.aruco.detectMarkers(
+            img,
+            arucoDict,
+            parameters=arucoParams
+        )
+
+        if len(corners) > 0:
+            ids = ids.flatten()
+            for (markerCorner, markerID) in zip(corners, ids):
+                corners = markerCorner.reshape((4, 2))
+                (topLeft, topRight, bottomRight, bottomLeft) = corners
+
+                topRight = (int(topRight[0]), int(topRight[1]))
+                bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+                bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+                topLeft = (int(topLeft[0]), int(topLeft[1]))
+
+                cv2.line(img, topLeft, topRight, (0, 255, 0), 2)
+                cv2.line(img, topRight, bottomRight, (0, 255, 0), 2)
+                cv2.line(img, bottomRight, bottomLeft, (0, 255, 0), 2)
+                cv2.line(img, bottomLeft, topLeft, (0, 255, 0), 2)
+
+                cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+                cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+                cv2.circle(img, (cX, cY), 4, (0, 0, 255), -1)
+
+                cv2.putText(img, str(markerID),
+                    (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5, (0, 255, 0), 2)
+
+                cv2.imshow("Image", img)
+
+        if cv2.waitKey(10) == 27 or not ret:
+            cap.release()
+            cv2.destroyAllWindows()
+            sys.exit(0)
---
+++ aruco_detection/generate_aruco.py
@@ -0,0 +1,71 @@
+import numpy as np
+import argparse
+import cv2
+import sys
+
+
+ARUCO_DICT = {
+	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+	"DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+	"DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+	"DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+	"DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+	"DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+	"DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+	"DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+	"DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+	"DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+	"DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+	"DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+	"DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+	"DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+	"DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+	"DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+	"DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+}
+
+
+def generate_marker(_id: int, output:str = None, type: int = cv2.aruco.DICT_ARUCO_ORIGINAL, tag_width: int = 300, show: bool = False):
+
+	if ARUCO_DICT.get(type, None) is None:
+		raise ValueError('Wrong type!')
+
+	if not output:
+		output = f'aruco_tag_{type}_{_id}.png'
+
+	arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[args["type"]])
+	tag = np.zeros((tag_width, tag_width, 1), dtype="uint8")
+	cv2.aruco.drawMarker(arucoDict, _id, tag_width, tag, 1)
+	cv2.imwrite(output, tag)
+	if show:
+		cv2.imshow("ArUCo Tag", tag)
+		cv2.waitKey(0)
+
+
+if __name__ == '__main__':
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-o", "--output", required=True,
+		help="path to output image containing ArUCo tag")
+	ap.add_argument("-i", "--id", type=int, required=True,
+		help="ID of ArUCo tag to generate")
+	ap.add_argument("-t", "--type", type=str,
+		default="DICT_ARUCO_ORIGINAL",
+		help="type of ArUCo tag to generate")
+	args = vars(ap.parse_args())
+
+	try:
+		generate_marker(
+			args["id"],
+			args["output"],
+			args["type"],
+			show=True
+		)
+	except ValueError:
+		print("[INFO] ArUCo tag of '{}' is not supported".format(
+			args["type"])
+		)
+		sys.exit(0)
